d82aa7ac600fba15160b4d9ed3dbf2b99c24121b
UgurNlbnt <ugur.nalbant617@gmail.com>
2026-01-30 22:01:12 +0300

    v0.5

---
+++ BakerWebUI/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BakerWebUI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DashboardController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = client.GetAsync("https://localhost:7109/api/Products/CountProduct");
+            var jsonData1 = await response.Result.Content.ReadAsStringAsync();
+            ViewBag.ProductCount = jsonData1;
+            return View();
+        }
+    }
+}
