93a7965f644363b696542e3eb46ce899adfdbee9
UgurNlbnt <ugur.nalbant617@gmail.com>
2026-03-10 09:59:39 +0300

    v0.8

--- BakerWebUI/Controllers/AdminSubscriberController.cs
+++ BakerWebUI/Controllers/AdminSubscriberController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BakerWebUI.Dtos.Subscribe;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BakerWebUI.Controllers
 {
     public class AdminSubscriberController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminSubscriberController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7109/api/Subscribe");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = Newtonsoft.Json.JsonConvert
+                    .DeserializeObject<List<ResultSubscribeDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View(new List<ResultSubscribeDto>());
+        }
+
+        public async Task<IActionResult> DeleteSubscriber(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync(
+                $"https://localhost:7109/api/Subscribe?id={id}");
+
+            return RedirectToAction("Index");
         }
     }
 }
--- BakerWebUI/Controllers/DefaultController.cs
+++ BakerWebUI/Controllers/DefaultController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BakerWebUI.Dtos.Subscribe;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BakerWebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(CreateSubscribeDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.PostAsJsonAsync("https://localhost:7109/api/Subscribe", dto);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Json(new { success = true, message = "Bültenimize başarıyla abone oldunuz." });
+            }
+
+            return Json(new { success = false, message = "Abonelik sırasında bir hata oluştu." });
         }
     }
 }
